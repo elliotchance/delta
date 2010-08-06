809f06107479bac2cfab21a120c50bb5dfa61ccd
Elliot Chance <elliot@chancemedia.com>
2010-08-06 14:22:28 +1000

    platform.h will works out more information like machine endianness.

--- src/delta/platform.h
+++ src/delta/platform.h
@@ -6,16 +6,54 @@
 #define _DELTA_PLATFORM_H 1
 
 
-#if defined(linux) || defined(__linux)
-#define DELTA_PLATFORM_LINUX 1
+//--------------------------------
+// Operating System
+//--------------------------------
+
+#if defined(linux) || \
+    defined(__linux)
+
+	#define DELTA_PLATFORM_LINUX 1
+
 #endif
 
-#if defined(macintosh) || defined(Macintosh) || defined(__APPLE__) || defined(__MACH__)
-#define DELTA_PLATFORM_MAC 1
+#if defined(macintosh) || \
+    defined(Macintosh) || \
+	defined(__APPLE__) || \
+    defined(__MACH__)
+
+	#define DELTA_PLATFORM_MAC 1
+
 #endif
 
-#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
-#define DELTA_PLATFORM_WINDOWS 1
+#if defined(_WIN32) || \
+    defined(_WIN64) || \
+    defined(__WIN32__) || \
+    defined(__TOS_WIN__) || \
+	defined(__WINDOWS__)
+
+	#define DELTA_PLATFORM_WINDOWS 1
+
+#endif
+
+
+//--------------------------------
+// Endianness
+//--------------------------------
+
+
+#if DELTA_PLATFORM_LINUX
+	#define DELTA_ENDIAN_UNKNOWN 1
+#endif
+
+
+#if DELTA_PLATFORM_MAC
+	#define DELTA_ENDIAN_UNKNOWN 1
+#endif
+
+
+#if DELTA_PLATFORM_WINDOWS
+	#define DELTA_ENDIAN_LITTLE 1
 #endif
 
 
