81f6b2618e418800a4ec975eb9619320ee5f0616
Elliot Chance <elliot@chancemedia.com>
2010-10-08 16:30:03 +1100

    strlen() is now faster when dealing only with DELTA_TYPE_STRING.

--- src/modules/core/string/strlen.c
+++ src/modules/core/string/strlen.c
@@ -12,6 +12,9 @@
  */
 DELTA_FUNCTION(strlen)
 {
+	if(DELTA_ARG0->type == DELTA_TYPE_STRING)
+		DELTA_RETURN_NUMBER(DELTA_ARG0->size);
+	
 	int release;
 	struct DeltaVariable *v = delta_cast_string(DELTA_ARG0, &release);
 	int r = v->size;
